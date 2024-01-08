4a147c92b1095c632aeba96b096eb8331e2fe9f6
Adi023 <adityapandhare050@gmail.com>
2024-01-08 23:42:34 +0530

    feat : added form based authentication and commented also use if needed

--- StoreManagement/src/main/java/com/adityaprojects/store/config/SecurityConfig.java
+++ StoreManagement/src/main/java/com/adityaprojects/store/config/SecurityConfig.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 
@@ -40,6 +42,26 @@ public class SecurityConfig {
 //		//InMemoryUserDetailsManager - is implementation class of UserDetailService
 //		return new InMemoryUserDetailsManager(normal,admin);
 //	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+//		http.authorizeRequests()
+//				.anyRequest()
+//				.authenticated()
+//				.and()
+//				.formLogin()
+//				.loginPage("login.html")
+//				.loginProcessingUrl("process-url")
+//				.defaultSuccessUrl("/dashboard")
+//				.failureUrl("error")
+//				.and()
+//				.logout()
+//				.logoutUrl("do-logout");
+
+				return http.build();
+	}
+	
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		
