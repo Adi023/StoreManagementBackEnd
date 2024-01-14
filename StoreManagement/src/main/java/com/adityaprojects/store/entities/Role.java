78f7c54192701fb16f8aa2e4988f41a6ee4e44a8
Adi023 <adityapandhare050@gmail.com>
2024-01-14 19:06:14 +0530

    feat : Role mode dto and repo added

---
+++ StoreManagement/src/main/java/com/adityaprojects/store/entities/Role.java
@@ -0,0 +1,25 @@
+package com.adityaprojects.store.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="roles")
+public class Role {
+
+	@Id
+	private String roleId;
+	private String roleName;
+}
