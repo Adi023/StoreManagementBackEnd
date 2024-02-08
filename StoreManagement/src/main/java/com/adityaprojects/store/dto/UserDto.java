28f7b9b62a5c5f7155e3c1b322c1e781d9017442
Adi023 <adityapandhare050@gmail.com>
2024-02-08 22:34:23 +0530

    refactor : dto message

--- StoreManagement/src/main/java/com/adityaprojects/store/dto/UserDto.java
+++ StoreManagement/src/main/java/com/adityaprojects/store/dto/UserDto.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 import com.adityaprojects.store.validate.ImageNameValid;
 
-//import jakarta.validation.constraints.Email;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
