bf8c6df97d125dbf3d289cd00ef4fe34fa333c2a
Adi023 <adityapandhare050@gmail.com>
2024-03-03 20:45:01 +0530

    feat : order entity added

---
+++ StoreManagement/src/main/java/com/adityaprojects/store/entities/Order.java
@@ -0,0 +1,59 @@
+package com.adityaprojects.store.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="orders")
+public class Order {
+
+	@Id
+    private String orderId;
+
+    //PENDING,DISPATCHED,DELIVERED,
+    //enum
+    private String orderStatus;
+
+    //NOT-PAID, PAID
+    //enum
+    //boolean- false=>NOTPAID  || true=>PAID
+    private String paymentStatus;
+
+    private int orderAmount;
+
+    @Column(length = 1000)
+    private String billingAddress;
+
+    private String billingPhone;
+
+    private String billingName;
+
+    private Date orderedDate;
+
+    private Date deliveredDate;
+
+    private  String razoryPayOrderId;
+    private String paymentId;
+
+//    //user
+//    @ManyToOne(fetch = FetchType.EAGER)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+//
+//    @OneToMany(mappedBy = "order", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+//    private List<OrderItem> orderItems = new ArrayList<>();
+}
