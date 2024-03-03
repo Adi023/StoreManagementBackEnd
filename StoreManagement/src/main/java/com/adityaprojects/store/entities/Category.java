dd112e980adcf5dda0f0ef803570bf88948c2cb7
Adi023 <adityapandhare050@gmail.com>
2024-03-04 01:08:45 +0530

    feat : category entity added

---
+++ StoreManagement/src/main/java/com/adityaprojects/store/entities/Category.java
@@ -0,0 +1,35 @@
+package com.adityaprojects.store.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+	 @Id
+	    @Column(name = "id")
+	    private String categoryId;
+
+	    @Column(name = "category_title", length = 60, nullable = false)
+	    private String title;
+
+	    @Column(name = "category_desc", length = 500)
+	    private String description;
+
+	    private String coverImage;
+	    // other attributes if you have...
+
+//	    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+//	    private List<Product> products = new ArrayList<>();
+}
